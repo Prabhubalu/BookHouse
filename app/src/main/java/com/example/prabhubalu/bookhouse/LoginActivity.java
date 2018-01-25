b2a346bd87b584aeb988a4db7f27a39d2c2819de
AmruthPillai <im.amruth@gmail.com>
2018-01-26 02:06:15 +0530

    restructured layout

--- app/src/main/java/com/example/prabhubalu/bookhouse/LoginActivity.java
+++ app/src/main/java/com/example/prabhubalu/bookhouse/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.example.prabhubalu.bookhouse;
 
 import android.content.Intent;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
      EditText usernameWrapper;
      EditText passwordWrapper;
@@ -37,7 +36,7 @@ public class login extends AppCompatActivity {
         reg_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(login.this,register.class);
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/prabhubalu/bookhouse/SplashActivity.java
+++ app/src/main/java/com/example/prabhubalu/bookhouse/SplashActivity.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class Splashscreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private static final int SPLASH_DURATION = 3000;
 
@@ -15,7 +14,7 @@ public class Splashscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.activity_splash);
 
         Handler myHandler = new Handler();
         myHandler.postDelayed(new Runnable() {
@@ -23,7 +22,7 @@ public class Splashscreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                    Intent intent = new Intent(Splashscreen.this,login.class);
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
                   startActivity(intent);
 
 
